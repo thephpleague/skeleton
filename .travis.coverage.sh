ef616202664fb79a7b9d2da98d317237ecd86c8f
Hans Ott <hansott@hotmail.be>
2016-09-19 08:29:46 +0200

    Don't use separate file for uploading code coverage

--- .travis.coverage.sh
+++
@@ -1,5 +0,0 @@
-set -x
-if [[ "$TRAVIS_PHP_VERSION" != 'hhvm' && "$TRAVIS_PHP_VERSION" != '7.0' ]] ; then
-    wget https://scrutinizer-ci.com/ocular.phar
-    php ocular.phar code-coverage:upload --format=php-clover coverage.clover
-fi
